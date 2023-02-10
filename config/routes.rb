7f769fd046870d27aca690efc9941f7e67e0a92e
nduchau <nduchau@DELL-GunKK.localdomain>
2023-02-10 23:30:59 +0700

    UI articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static_pages#home"
+  root to: "static_pages#home"
 end
